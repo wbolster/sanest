4ea647fa9fdddde9f4705a3404c7309bc7e19da8
wouter bolsterlee <wouter@bolsterl.ee>
2017-05-19 17:07:20 +0200

    basic packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sanest',
+    description="sane nested dictionaries and lists",
+    version='0.0.1',
+    py_modules='sanest',
+    author="wouter bolsterlee",
+    author_email="wouter@bolsterl.ee",
+    url='https://github.com/wbolster/sanest',
+    license="BSD",
+)
