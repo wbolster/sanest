d69357b5d3370fe940f325d3d2182c149239221c
wouter bolsterlee <wouter@bolsterl.ee>
2017-05-19 23:19:32 +0200

    implement nested and type-checking dict lookups

--- sanest.py
+++ sanest.py
@@ -4,12 +4,56 @@ sanest, sane nested dictionaries and lists
 
 import collections.abc
 
+TYPES = [bool, float, int, str]
+
+
+def normalise_path(path):
+    if isinstance(path, str):
+        return [path]
+    if isinstance(path, (tuple, list)):
+        return path
+    raise TypeError("invalid path")
+
+
+def lookup(obj, *, path, value_type=None):
+    path = normalise_path(path)
+    if not path:
+        raise ValueError("empty path")
+    if value_type is not None and value_type not in TYPES:
+        raise TypeError(
+            "type must be one of {}"
+            .format(', '.join(t.__name__ for t in TYPES)))
+    for component in path:
+        if isinstance(component, str):
+            if not isinstance(obj, Mapping):
+                raise NotImplementedError
+        elif isinstance(component, int):
+            raise NotImplementedError('lists')
+        obj = obj[component]
+    if value_type is not None and not isinstance(obj, value_type):
+        raise ValueError(
+            "requested {.__name__}, got {.__name__}: {!r}"
+            .format(value_type, type(obj), obj))
+    return obj
+
 
 class Mapping(collections.abc.Mapping):
     def __getitem__(self, key):
-        if not isinstance(key, str):
-            raise TypeError("key must be a string")
-        return self._data[key]
+        if isinstance(key, str):  # basic lookup
+            return self._data[key]
+        if isinstance(key, slice):  # typed lookup
+            if key.step is not None:
+                raise TypeError("invalid key: slice cannot contain step value")
+            value_type = key.stop
+            key = key.start
+            return lookup(
+                self, path=normalise_path(key), value_type=value_type)
+        if isinstance(key, tuple):  # nested lookup
+            return lookup(self, path=key)
+        # todo: factor out slice parsing and path normalisation so that
+        # things like d['a','b','c':str] do not depend on recursive
+        # lookup() calls.
+        raise TypeError("invalid key: {!r}".format(key))
 
     def __len__(self):
         return len(self._data)
@@ -21,12 +65,12 @@ class Mapping(collections.abc.Mapping):
 class MutableMapping(Mapping, collections.abc.MutableMapping):
     def __setitem__(self, key, value):
         if not isinstance(key, str):
-            raise TypeError("key must be a string")
+            raise TypeError("invalid key: {!r}".format(key))
         self._data[key] = value
 
     def __delitem__(self, key):
         if not isinstance(key, str):
-            raise TypeError("key must be a string")
+            raise TypeError("invalid key: {!r}".format(key))
         del self._data[key]
 
 
--- test_sanest.py
+++ test_sanest.py
@@ -12,14 +12,22 @@ import pytest
     [123, None, b"foo", True])
 def test_dict_string_keys_only(key):
     d = sanest.dict()
-    with pytest.raises(TypeError):
+
+    with pytest.raises(TypeError) as excinfo:
         d[key]
-    with pytest.raises(TypeError):
+    assert str(excinfo.value).startswith("invalid key: ")
+
+    with pytest.raises(TypeError) as excinfo:
         d[key] = key
-    with pytest.raises(TypeError):
+    assert str(excinfo.value).startswith("invalid key: ")
+
+    with pytest.raises(TypeError) as excinfo:
         d.get(key)
-    with pytest.raises(TypeError):
+    assert str(excinfo.value).startswith("invalid key: ")
+
+    with pytest.raises(TypeError) as excinfo:
         key in d
+    assert str(excinfo.value).startswith("invalid key: ")
 
 
 def test_dict_basics():
@@ -30,3 +38,33 @@ def test_dict_basics():
     d.setdefault('b', 2)
     assert d['a'] == 1
     assert d['b'] == 2
+
+
+def test_dict_typed_lookup():
+    d = sanest.dict()
+    d['a'] = 'aaa'
+    d['b'] = 2
+
+    assert d['a':str] == 'aaa'
+    assert d['b':int] == 2
+
+    with pytest.raises(ValueError) as excinfo:
+        d['a':int]
+    assert str(excinfo.value) == "requested int, got str: 'aaa'"
+
+    with pytest.raises(TypeError) as excinfo:
+        d['nonexistent':bytes]
+    assert str(excinfo.value) == "type must be one of bool, float, int, str"
+
+    with pytest.raises(TypeError) as excinfo:
+        d['a':int:str]
+    assert str(excinfo.value) == "invalid key: slice cannot contain step value"
+
+
+def test_dict_path_lookup():
+    # todo: nice way to constructed nested dicts
+    d = sanest.dict()
+    d['a'] = sanest.dict()
+    d['a']['b'] = 123
+    assert d['a', 'b'] == 123
+    assert d['a', 'b':int] == 123
