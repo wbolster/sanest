c82ff5b1e07f2427af77b4ac9fb49f282c80e5ef
wouter bolsterlee <wouter@bolsterl.ee>
2017-05-21 00:35:41 +0200

    expand tests

--- test_sanest.py
+++ test_sanest.py
@@ -47,6 +47,9 @@ def test_dict_typed_lookup():
 
     assert d['a':str] == 'aaa'
     assert d['b':int] == 2
+    assert d.get('a', type=str) == 'aaa'
+    assert d.get('c', type=str) is None
+    assert d.get('c', default=123, type=str) == 123
 
     with pytest.raises(ValueError) as excinfo:
         d['a':int]
@@ -61,10 +64,23 @@ def test_dict_typed_lookup():
     assert str(excinfo.value) == "invalid key: slice cannot contain step value"
 
 
-def test_dict_path_lookup():
+def test_dict_nested_lookup():
     # todo: nice way to constructed nested dicts
     d = sanest.dict()
     d['a'] = sanest.dict()
     d['a']['b'] = 123
+
     assert d['a', 'b'] == 123
     assert d['a', 'b':int] == 123
+    assert ('a', 'b') in d
+    assert ['a', 'b'] in d
+    assert ['c', 'd'] not in d
+    assert d.get(('a', 'b')) == 123
+
+    with pytest.raises(TypeError) as excinfo:
+        d['a', 123, True]
+    assert str(excinfo.value).startswith("invalid key: ")
+
+    with pytest.raises(TypeError) as excinfo:
+        d.get([])
+    assert str(excinfo.value) == "invalid key: empty path"
