dfb2eb0d8cbb54a8516702ae3ef4a122f67b5f95
wouter bolsterlee <wouter@bolsterl.ee>
2017-06-20 20:50:26 +0200

    fix py_modules definition in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sanest',
     description="sane nested dictionaries and lists",
     version='0.0.1',
-    py_modules='sanest',
+    py_modules=['sanest'],
     author="wouter bolsterlee",
     author_email="wouter@bolsterl.ee",
     url='https://github.com/wbolster/sanest',
