130386b69b0f8fcbcd7a2735f61189d132878de8
wouter bolsterlee <wouter@bolsterl.ee>
2017-07-02 20:52:02 +0200

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -10,4 +10,14 @@ setup(
     author_email="wouter@bolsterl.ee",
     url='https://github.com/wbolster/sanest',
     license="BSD",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
