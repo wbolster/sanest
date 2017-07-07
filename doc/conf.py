adbeebbda02921663715e3690e3976055e473194
wouter bolsterlee <wouter@bolsterl.ee>
2017-07-07 21:34:45 +0200

    use readthedocs sphinx theme for local doc builds
    ...since it makes the local builds look like the hosted docs.

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,5 @@
+import os
+
 master_doc = 'index'
 project = 'sanest'
 copyright = 'wouter bolsterlee (@wbolster)'
@@ -5,3 +7,8 @@ copyright = 'wouter bolsterlee (@wbolster)'
 extensions = [
     'sphinx.ext.autodoc',
 ]
+
+if 'READTHEDOCS' not in os.environ:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
